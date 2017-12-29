165fadd7118168faf7d678ebd9bcd87d41b5d683
rlin <rlin@thoughtworks.com>
2017-12-29 16:47:52 +0800

    [@boydfd] delete useless log.

--- docker/build.sh
+++ docker/build.sh
@@ -11,3 +11,7 @@ cd -
 cp -f ../build/libs/eureka-client-0.0.1-SNAPSHOT.jar ./$application_jar
 
 docker build -t $application_name .
+
+docker tag $application_name 172.16.35.12:5000/$application_name
+
+docker push 172.16.35.12:5000/$application_name
